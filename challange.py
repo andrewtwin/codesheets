b7c7c72a46fb3a9d7b1e86adf4f55eb5cd1c0009
Andrew Twin <andrewtwin@gmail.com>
2025-01-05 13:27:08 +1100

    refactoring

--- challange.py
+++ challange.py
@@ -3,17 +3,23 @@ import json
 import sys
 
 def load_sheet(code_file: str) -> dict:
-    with open(code_file, 'r') as file:
+    with open(code_file, 'r', encoding='UTF8') as file:
         return json.load(file)
-    
-def main(sheet_name):
-    codes = load_sheet(sheet_name)
+
+def pick_code(codes: dict) -> dict:
+    sheet_name = codes['sheet_name']
     row_num = secrets.choice(list(codes['codes']))
     row = codes['codes'][row_num]
     col = secrets.choice(list(row))
-    print(f"Sheet {codes['sheet_name']}")
-    print(f"Index {col}{row_num}")
-    print(f"Code  {row[col]}")
+    code = row[col]
+    return dict([('sheet', sheet_name), ('index', f"{col}{row_num}"), ('code', code)])
+
+def main(sheet_name):
+    codes = load_sheet(sheet_name)
+    code = pick_code(codes)
+    print(f"Sheet {code['sheet']}")
+    print(f"Index {code['index']}")
+    print(f"Code  {code['code']}")
 
 if __name__ == "__main__":
     main(sheet_name = sys.argv[1])
